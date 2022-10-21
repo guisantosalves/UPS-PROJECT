bbf3f1e558247440384f16c2eafd448c0fa82a42
guisantosalves <workguisantos@gmail.com>
2022-10-21 17:15:58 -0400

    starting queries gql and installing apollo

--- App.tsx
+++ App.tsx
@@ -3,18 +3,27 @@ import { TailwindProvider } from "tailwind-rn";
 import utilities from "./tailwind.json";
 import { useTailwind } from "tailwind-rn";
 import RootNavigator from "./navigator/RootNavigator";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 // screens
 import CustomerScreen from "./screens/CustumerScreen";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:5001/api/zeroed-dog',
+    cache: new InMemoryCache(),
+  });
+
   return (
     // @ts-ignore - tailwind is missing type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
