8d3816fdbdeb9131a4e25dfb3fabeff5d18e60e2
guisantosalves <workguisantos@gmail.com>
2022-10-21 11:38:48 -0400

    setting the tab navigator and styling it

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,25 +1,60 @@
 import * as React from "react";
-import { View } from "react-native"; 
+import { View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "@rneui/themed";
 
 // screens
 import CustomerScreen from "../screens/CustumerScreen";
 import OrdersScreen from "../screens/OrdersScreen";
+import { useNavigation } from "@react-navigation/native";
 
 export type tabStackParamList = {
-    Customers: undefined;
-    Orders: undefined;
-}
+  Customers: undefined;
+  Orders: undefined;
+};
 
 const Tab = createBottomTabNavigator<tabStackParamList>();
 
 const TabNavigator = () => {
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name="Customers" component={CustomerScreen}/>
-            <Tab.Screen name="Orders" component={OrdersScreen}/>
-        </Tab.Navigator>
-    )
-}
+  const navigation = useNavigation();
+
+  // before render the screen
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#59c1cc",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color }) => {
+          if (route.name == "Customers") {
+            return (
+              <Icon
+                name="users"
+                type="entypo"
+                color={focused ? "##59c1cc" : "gray"}
+              />
+            );
+          } else if (route.name == "Orders") {
+            return (
+              <Icon
+                name="box"
+                type="entypo"
+                color={focused ? "##59c1cc" : "gray"}
+              />
+            );
+          }
+        },
+      })}
+    >
+      <Tab.Screen name="Customers" component={CustomerScreen} />
+      <Tab.Screen name="Orders" component={OrdersScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default TabNavigator;
